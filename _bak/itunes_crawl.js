14ad41e0b591e5d2faecae88607a063d6bb21277
mattzeiger <matt.zeiger@gmail.com>
2015-02-06 17:40:48 -0800

    Movie App iTunes parser work

---
+++ _bak/itunes_crawl.js
@@ -0,0 +1,103 @@
+var connection = require('express-myconnection');
+var mysql = require('mysql');
+var request = require('request').defaults({
+		maxRedirects:20,
+		jar: true
+	});
+
+
+app.use(
+	connection(mysql, {
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		port: 3306,
+		database: 'movieapp'
+	}, 'pool')
+);
+
+
+/* 
+
+//module.exports = app;
+//start server
+app.listen(app.get('port'), function() {
+	console.log("App is listening on port " + app.get('port'));
+});
+*/
+
+	//'X-Apple-Store-Front: 143441-1,28'
+	//https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewGrouping?cc=us&id=39
+	var options = {
+		urL : 'file://localhost/Applications/MAMP/htdocs/movieapp/itunes/response.json',
+		headers: {
+			'X-Apple-Store-Front' : '143441-1,28'
+		}
+	};
+	request('file://localhost/Applications/MAMP/htdocs/movieapp/itunes/response.json', function(error, response, body) {
+		console.log("CHECK A");
+		if (!error) {
+			console.log("NO ERROR"); // && response.statusCode == 200
+			var holdar = body.split('its.serverData=');
+			var new_response = holdar[1];
+			hold_ar = new_response.split("</script>");
+			var json_raw_response = hold_ar[0];
+
+			var json_response = JSON.parse(json_raw_response);			
+			console.log(json_raw_response);
+			console.log(json_response['storePlatformData']['lockup-room']['version']); //['results']
+			console.log("HERE A");
+
+			/*
+			// cheerio parses the raw HTML of the response into a jQuery-like object for easy parsing
+			var $ = cheerio.load(body);
+
+			// we're specifically looking for an ID on the page we need to log in, which looks like this: 
+			// <input type="hidden" name="authURL" value="1388775312720.+vRSN6us+IhZ1qOSlo8CyAS/ZJ4=">
+			data.authURL = $("input[name='authURL']").attr("value");
+
+			request.post("https://signup.netflix.com/Login", { form: data }, function(err, resp, body) {
+				if (err) { throw err; }
+				console.log("Successfully logged in.");
+
+				// when you first log in, NF prompts you to use the default profile or create a new one
+				// this function gets us through that interstitial page
+				getProfileCookie(function() {
+					for (var c = 0; c < 100000; c += 1) {
+						getGenre(c);
+					}
+				});
+			});
+			
+			$holdar = explode('its.serverData=', $response);
+			$new_response = $holdar[1];
+			$hold_ar = explode("</script>", $new_response);
+			$json_raw_response = $hold_ar[0];
+
+			$json_response = json_decode($json_raw_response);
+			if ($json_response == NULL) {
+				echo "Not Decoded!";
+				exit();
+			}
+
+			$movie_results = $json_response->{'storePlatformData'}->{'lockup-room'}->{'results'};
+			foreach ($movie_results as $key=>$value) {
+				$prices = array();
+				$offers = $movie_results->{$key}->{'offers'};
+				echo $movie_results->{$key}->{'name'}." - ";//." | ".implode(" | ", $prices)."\r\n";
+
+				foreach ($offers as $akey=>$avalue) {
+					$prices[$avalue->{'actionText'}->{'short'}] = $avalue->{'priceFormatted'};	
+					echo $avalue->{'actionText'}->{'short'}." : ".$avalue->{'priceFormatted'}." | ";
+				}
+				echo "\r\n";
+				
+			}
+			*/
+		} else {
+			//console.log('error: '+ response.statusCode)
+        	console.log(body)
+		}
+	});
+
+
--- itunes_crawl.js
+++ itunes_crawl.js
@@ -1,124 +1,134 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-
-var app = express();
-var connection = require('express-myconnection');
 var mysql = require('mysql');
-var request = require('request').defaults({
-		maxRedirects:20,
-		jar: true
-	});
+var request = require('request');
+var unique = require('array-unique'); //used to unique an array //unique(['a', 'b', 'c', 'c']);
 
-
-app.set('port', process.env.PORT || 8080);
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use(
-	connection(mysql, {
-		host: 'localhost',
-		user: 'root',
-		password: 'root',
-		port: 3306,
-		database: 'movieapp'
-	}, 'pool')
+var INITIAL_URL = 'http://localhost:8080/response.html'; //https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewGrouping?cc=us&id=39'; //Movies Page on iTunes
+var connection = mysql.createConnection(
+    {
+      host     : 'localhost',
+      user     : 'root',
+      password : 'root',
+      database : 'movieapp',
+    }
 );
 
-app.use('/', routes);
 
-/* 
-
-//module.exports = app;
-//start server
-app.listen(app.get('port'), function() {
-	console.log("App is listening on port " + app.get('port'));
-});
-*/
-
-	//'X-Apple-Store-Front: 143441-1,28'
-	//https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewGrouping?cc=us&id=39
+function parseiTunesURL(url) {
+	console.log("Parsing URL:" + url);			
 	var options = {
-		urL : 'file://localhost/Applications/MAMP/htdocs/movieapp/itunes/response.json',
-		headers: {
-			'X-Apple-Store-Front' : '143441-1,28'
+		url : url, //url we are loading
+		headers : {
+			'X-Apple-Store-Front' : '143441-1,28' //required so that iTunes accepts the request
 		}
 	};
-	request('file://localhost/Applications/MAMP/htdocs/movieapp/itunes/response.json', function(error, response, body) {
-		console.log("CHECK A");
-		if (!error) {
-			console.log("NO ERROR"); // && response.statusCode == 200
-			var holdar = body.split('its.serverData=');
-			var new_response = holdar[1];
-			hold_ar = new_response.split("</script>");
-			var json_raw_response = hold_ar[0];
-
-			var json_response = JSON.parse(json_raw_response);			
-			console.log(json_raw_response);
-			console.log(json_response['storePlatformData']['lockup-room']['version']); //['results']
-			console.log("HERE A");
-
-			/*
-			// cheerio parses the raw HTML of the response into a jQuery-like object for easy parsing
-			var $ = cheerio.load(body);
-
-			// we're specifically looking for an ID on the page we need to log in, which looks like this: 
-			// <input type="hidden" name="authURL" value="1388775312720.+vRSN6us+IhZ1qOSlo8CyAS/ZJ4=">
-			data.authURL = $("input[name='authURL']").attr("value");
-
-			request.post("https://signup.netflix.com/Login", { form: data }, function(err, resp, body) {
-				if (err) { throw err; }
-				console.log("Successfully logged in.");
-
-				// when you first log in, NF prompts you to use the default profile or create a new one
-				// this function gets us through that interstitial page
-				getProfileCookie(function() {
-					for (var c = 0; c < 100000; c += 1) {
-						getGenre(c);
-					}
-				});
-			});
-			
-			$holdar = explode('its.serverData=', $response);
-			$new_response = $holdar[1];
-			$hold_ar = explode("</script>", $new_response);
-			$json_raw_response = $hold_ar[0];
-
-			$json_response = json_decode($json_raw_response);
-			if ($json_response == NULL) {
-				echo "Not Decoded!";
-				exit();
-			}
-
-			$movie_results = $json_response->{'storePlatformData'}->{'lockup-room'}->{'results'};
-			foreach ($movie_results as $key=>$value) {
-				$prices = array();
-				$offers = $movie_results->{$key}->{'offers'};
-				echo $movie_results->{$key}->{'name'}." - ";//." | ".implode(" | ", $prices)."\r\n";
-
-				foreach ($offers as $akey=>$avalue) {
-					$prices[$avalue->{'actionText'}->{'short'}] = $avalue->{'priceFormatted'};	
-					echo $avalue->{'actionText'}->{'short'}." : ".$avalue->{'priceFormatted'}." | ";
+	request(options, function(error, response, body) {			
+			if (!error && response.statusCode == 200) {				
+				var holdar = body.split('its.serverData=');
+				var new_response = '';
+				if (holdar.length > 1) {
+					new_response = holdar[1];			
+					hold_ar = new_response.split("</script>");
+					new_response = hold_ar[0];
 				}
-				echo "\r\n";
-				
+
+				if (new_response != '') {
+					var json_response = {};
+					try {
+						json_response = JSON.parse(new_response);						
+						if (json_response == null) {
+							console.log("JSON parsing error");
+						}
+					} catch (e) {
+						console.log('JSON parsing error');
+					}
+
+					//loop through all of the Movies on this page
+					var movie_results = json_response['storePlatformData']['lockup-room']['results'];
+					for (var key in movie_results) {
+						var prices = new Array();
+						var offers = movie_results[key]['offers'];
+						
+							//console.log(movie_results[key]['name']);					
+						//see if a movie exists with this name, alt_name and this year
+						var movie_name = movie_results[key]['name'];
+						var movie_year = {movie_results[key]['releaseDate'].split('-')}[0];
+						console.log('movie_year:' + movie_year);
+						connection.query('SELECT * FROM movie WHERE (title = ? OR alt_title = ?) AND year = ?;', {movie_name, movie_name, movie_year}, function (err,rows, fields) {
+
+
+						
+							//it doesn't? then create it	
+								
+							/*echo $movie_results->{$key}->{'name'}." - ";//." | ".implode(" | ", $prices)."\r\n";
+
+							foreach ($offers as $akey=>$avalue) {
+								$prices[$avalue->{'actionText'}->{'short'}] = $avalue->{'priceFormatted'};	
+								echo $avalue->{'actionText'}->{'short'}." : ".$avalue->{'priceFormatted'}." | ";
+							}
+							echo "\r\n";
+
+							var moviedata = {
+								name: input.inputName,
+								address: input.inputAddress,
+								email: input.inputEmail,
+								phone: input.inputPhone
+							};
+							
+							
+							//create a new movie record
+							console.log("Creating a new movie");						
+															
+							req.getConnection(function(err,connection){			
+								
+								var query = connection.query('INSERT INTO customer set ?', data, function(err,rows) {
+									
+									if (err) {
+										console.log("Error Selecting : %s ",err );
+									}
+									
+									res.redirect("/customers?status=created");												
+								});
+							});		
+						});
+
+						
+
+						*/
+					} //end for loop				
+
+					//look for other Pages (i.e. URL's with MZStore.woa
+						//add them to url_queue
+
+				} //if the response is not empty...
+
+			} else {
+				//console.log('error: '+ response.statusCode)
+				console.log("ERROR");
+	        	console.log(error)
 			}
-			*/
-		} else {
-			//console.log('error: '+ response.statusCode)
-        	console.log(body)
-		}
-	});
+	});	
+}
+ 
+connection.connect();
 
+var url_queue = new Array(); // 
+var queryString = 'SELECT * FROM itunes_urlq'; //load pre-existing url q list from DB 
+connection.query(queryString, function(err, rows, fields) {
+    if (err) throw err;
+ 
+ 	if (rows.length > 0) {
+    	for (var i in rows) {
+    		url_queue.push(rows[i].url);	        
+	    }
+	} else {
+		//if empty, start with INITIAL_URL
+		url_queue.push(INITIAL_URL);		
+	}
+});
+		
 
+for(var i = 0;i<url_queue.length;i++) {
+	parseiTunesURL(url_queue[i]);
+}
+ 
+connection.end();
