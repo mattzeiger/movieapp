721bde317eed2f3a28557604ae0d45719a73aa41
mattzeiger <matt.zeiger@gmail.com>
2015-02-08 21:16:24 -0800

    iTunes crawling

--- itunes_crawl.js
+++ itunes_crawl.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var request = require('request');
 var unique = require('array-unique'); //used to unique an array //unique(['a', 'b', 'c', 'c']);
+var dateformatl = require("date-format-lite");
 
 var INITIAL_URL = 'http://localhost:8080/response.html'; //https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewGrouping?cc=us&id=39'; //Movies Page on iTunes
 var connection = mysql.createConnection(
@@ -45,58 +46,10 @@ function parseiTunesURL(url) {
 					//loop through all of the Movies on this page
 					var movie_results = json_response['storePlatformData']['lockup-room']['results'];
 					for (var key in movie_results) {
-						var prices = new Array();
-						var offers = movie_results[key]['offers'];
-						
-							//console.log(movie_results[key]['name']);					
-						//see if a movie exists with this name, alt_name and this year
-						var movie_name = movie_results[key]['name'];
-						var movie_year = {movie_results[key]['releaseDate'].split('-')}[0];
-						console.log('movie_year:' + movie_year);
-						connection.query('SELECT * FROM movie WHERE (title = ? OR alt_title = ?) AND year = ?;', {movie_name, movie_name, movie_year}, function (err,rows, fields) {
 
+						parseMovie(movie_results[key]);
 
-							//it does, then check to see if there is an iTunes record for it
-								//there is, then update it
-								//there isn't, then create it
-								
-							//it doesn't? then create it	
-
-							/*echo $movie_results->{$key}->{'name'}." - ";//." | ".implode(" | ", $prices)."\r\n";
-
-							foreach ($offers as $akey=>$avalue) {
-								$prices[$avalue->{'actionText'}->{'short'}] = $avalue->{'priceFormatted'};	
-								echo $avalue->{'actionText'}->{'short'}." : ".$avalue->{'priceFormatted'}." | ";
-							}
-							echo "\r\n";
-
-							var moviedata = {
-								name: input.inputName,
-								address: input.inputAddress,
-								email: input.inputEmail,
-								phone: input.inputPhone
-							};
-							
-							
-							//create a new movie record
-							console.log("Creating a new movie");						
-															
-							req.getConnection(function(err,connection){			
-								
-								var query = connection.query('INSERT INTO customer set ?', data, function(err,rows) {
-									
-									if (err) {
-										console.log("Error Selecting : %s ",err );
-									}
-									
-									res.redirect("/customers?status=created");												
-								});
-							});		
-						});
-
-						
-
-						*/
+										
 					} //end for loop				
 
 					//look for other Pages (i.e. URL's with MZStore.woa
@@ -111,7 +64,106 @@ function parseiTunesURL(url) {
 			}
 	});	
 }
- 
+
+function parseMovie(movieResults) {
+	var prices = new Array();
+	var offers = movieResults['offers'];
+	
+	//console.log(movie_results[key]['name']);					
+	//see if a movie exists with this name, alt_name and this year
+
+	//console.log('releaseDate:' + movie_results[key]['releaseDate']);
+	if (movieResults['releaseDate'] == null) {							
+		var movie_year = 0;
+		//var now = new Date();
+		//now.format("YYYY");
+		//console.log("Release date null for " + movie_results[key]['name'] + ' so we are setting it to :' + movie_year);	
+	} else {
+		var movie_year = movieResults['releaseDate'].date("YYYY");
+	}
+
+	/*
+	echo $movie_results->{$key}->{'name'}." - ";//." | ".implode(" | ", $prices)."\r\n";
+
+			foreach ($offers as $akey=>$avalue) {
+				$prices[$avalue->{'actionText'}->{'short'}] = $avalue->{'priceFormatted'};	
+				echo $avalue->{'actionText'}->{'short'}." : ".$avalue->{'priceFormatted'}." | ";
+			}
+			echo "\r\n";
+	*/
+
+	var movie_name = movieResults['name'];
+	//normalize the movie title (removing additional things like "(2015) or (Watch Now)"")	
+	
+//if it ends on a right ) and the corresponding left ( doesn't start the string, then cut it from the string
+	// if there is a hyphen - directly to the left of the ( then cut that too
+//if there is 'Anniversary' to the right of the - then cut everything to the right of the hyphen -
+
+
+	var query = connection.query('SELECT movie.id, movie.title, movie.year, moviesource.id as moviesourceid FROM movie LEFT JOIN moviesource ON movie.id = moviesource.movie_id AND moviesource.source = \'itunes\' WHERE (movie.title = ? OR movie.alt_title = ?) ORDER BY movie.year DESC;', [movie_name, movie_name], function (err,rows, fields) {
+		if (err) throw err;
+		//see if there is more than one...				
+		if (rows.length > 1) {
+			if (movie_year > 0) {
+				//loop through each movie that matched this title
+					//check the release year of it against the release date year of the record we have 
+			} else {
+				//we're going with the first guy
+			}
+		} else if (rows.length == 1) {
+			//it does, then check to see if there is an iTunes record for it
+			if (rows[0].moviesourceid > 0) {
+				//there is, then update it
+			} else {
+
+				//there isn't, then create the iTunes moviesource record for this movie
+				/*
+				var data = {
+					movie_id : rows[0].id,
+					source : 'itunes',
+					source_id : '',
+					free : ''				[1/0]	--denotes if it is truly free
+					subscription_free 	[1/0]	--denotes free with subscription (i.e. hulu + or Netflix)
+					rental_price		[2.99]
+					purchase_price		[9.99]
+					rental_price_hd		[2.99]
+					purchase_price_hd	[9.99]
+					cc					[1/0]
+					coming_soon			[1/0]
+				};
+				connection.query('INSERT INTO moviesource set ? ', data, function(err, rows, fields) {
+
+				});
+				*/
+
+
+			}
+		} else if (rows.length == 0) {
+
+			var moviedata = {
+				title : '',
+				alt_title : '',
+				year : '',
+				mpaa : '',				//[0=NR,1=G,2=PG,3=PG-13,4=R,5=NC-17]
+				description : '',
+				genre_primary : '',
+				director_id : '',
+				duration : '',			//	[120]
+				studio : '',
+				budget : '',
+				release_date : '',
+				box_office : ''
+			};										
+			//it doesn't? then create the movie record
+			var query = connection.query('INSERT INTO movie set ? ', moviedata, function(err,rows,fields) {
+
+			});			
+		}
+	
+	});
+}
+
+
 connection.connect();
 
 var url_queue = new Array(); // 
@@ -119,7 +171,7 @@ var queryString = 'SELECT * FROM itunes_urlq'; //load pre-existing url q list fr
 connection.query(queryString, function(err, rows, fields) {
     if (err) throw err;
  
- 	if (rows.length > 0) {
+ 	if (rows.length > 0) { 	
     	for (var i in rows) {
     		url_queue.push(rows[i].url);	        
 	    }
@@ -127,11 +179,12 @@ connection.query(queryString, function(err, rows, fields) {
 		//if empty, start with INITIAL_URL
 		url_queue.push(INITIAL_URL);		
 	}
+
+	for(var i = 0;i<url_queue.length;i++) {	
+		parseiTunesURL(url_queue[i]);
+	}
 });
 		
-
-for(var i = 0;i<url_queue.length;i++) {
-	parseiTunesURL(url_queue[i]);
-}
  
-connection.end();
+//console.log('closing connection');
+//connection.end();
