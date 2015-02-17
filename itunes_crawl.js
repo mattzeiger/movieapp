8d330f31bc5e5e1042fffd437d89bfa635b990e6
mattzeiger <matt.zeiger@gmail.com>
2015-02-17 02:53:49 -0800

    edition support

--- itunes_crawl.js
+++ itunes_crawl.js
@@ -165,73 +165,145 @@ function parseMovie(movieResults, callback) {
 	}	
 
 	var movie_name = movieResults['name'];
-	movie_name = normalizeMovieTitle(movie_name);
 
-	var query = connection.query('SELECT movie.id, movie.title, movie.year, moviesource.id as moviesourceid FROM movie LEFT JOIN moviesource ON movie.id = moviesource.movie_id AND moviesource.source = \'itunes\' WHERE (movie.title = ? OR movie.alt_title = ?) ORDER BY movie.year DESC;', [movie_name, movie_name], function (err,rows, fields) {
-		if (err) throw err;
-		var movierow;
-		if (rows.length == 1) {
-			movierow = rows[0];
-		} else if (rows.length > 1) { //see if there is more than one...
-			if (movie_year > 0) {
-				//loop through each movie that matched this title
-				for(var i = 0;i<rows.length;i++) {
-					//check the release year of it against the release date year of the record we have 
-					if (rows[i].year == movie_year) {
-						movierow = rows[i];
-					}
-				}
-			} //otherwise, we're going with the first guy
-		} 
-		if (movierow) {
-			//console.log('Found a movie');
-			//it does, then check to see if there is an iTunes record for it
-			if (movierow.moviesourceid > 0) {
-				//there is, then update it (specifically the moviesource info)
-				//Update the moviesource info				
-				updateMovieSource(movieResults, movierow.id, movierow.moviesourceid, callback);
-			} else {
-				console.log('there is not, then create the iTunes moviesource record for this movie');
-				updateMovieSource(movieResults, movierow.id, 0, callback);
-			}
-		} else {
-			//console.log('Create the movie');
-			var contentRating = 0;
-			if (movieResults['contentRating'].name == 'G') {
-				contentRating = 1;
-			} else if (movieResults['contentRating'].name == 'PG') {
-				contentRating = 2;
-			} else if (movieResults['contentRating'].name == 'PG-13') {
-				contentRating = 3;
-			} else if (movieResults['contentRating'].name == 'R') {
-				contentRating = 4;
-			} else if (movieResults['contentRating'].name == 'NC-17') {
-				contentRating = 5;
-			}
-
-			var moviedata = {
-				title : movie_name,
-				alt_title : '',
-				year : movie_year,
-				mpaa : contentRating,				//[0=NR,1=G,2=PG,3=PG-13,4=R,5=NC-17]
-				description : '',
-				genre_primary : '',
-				director_id : 0,
-				duration : '',			//	[120]
-				studio : '',
-				budget : '',
-				release_date : movieResults['releaseDate'],
-				box_office : ''
-			};
-			//console.log(moviedata);										
-			//it doesn't? then create the movie record
-			var query = connection.query('INSERT INTO movie set ? ', moviedata, function(err,result,fields) {				
-				//create the moviesource record as well...				
-				updateMovieSource(movieResults, result.insertId, 0, callback);				
-			});			
-		}
+	//see if this is a director's cut, extended edition, or anniversary edition
+	var isEdition = detectEdition(movie_name); //blank if not, otherwise the descriptors comma-separated
 	
-	});
+	//normalize the title
+	var alt_title = movie_name;	
+	movie_name = normalizeMovieTitle(movie_name);
+	if (alt_title == movie_name) {
+		alt_title = '';
+	}
+
+	//first, attempt to find the movie_id based on movieResults['id'] (the iTunes unique key)
+	var query = connection.query('SELECT movie.id, movie.title, movie.year, movie.edition_label, movie.parent_id, moviesource.id as moviesourceid FROM movie LEFT JOIN moviesource ON movie.id = moviesource.movie_id AND moviesource.source = \'itunes\' WHERE moviesource.source_id = ? ', movieResults['id'], function (err,rows,fields) {
+		if (err) throw err;		
+		if (rows.length > 0) { //we found the movie, based on the iTunes key!
+			var movierow;
+			movierow = rows[0];
+
+			//go ahead and correct the title and the edition fields if its an edition
+			if (isEdition != '') {
+				//Look for the Parent: see if this title and approximate year match another movie (i.e. attempt to find the parent movie for this edition)
+				var query = connection.query("SELECT id FROM movie WHERE (movie.title = ? OR movie.alt_title = ?) AND (movie.year >= (?-1) AND movie.year <= (?+1)) AND parent_id=0 AND (edition_label = '' OR edition_label IS NULL) AND movie.id <> ? ", [movie_name, movie_name, movierow.year, movierow.year, movierow.id], function (err,rows) {
+					if (err) throw err;		
+					var parent_id = movierow.parent_id;
+					if (rows.length > 0 && parent_id == 0) {
+						parent_id = rows[0].id;						
+					}
+
+					var query = connection.query('UPDATE movie SET movie.title = ?, alt_title = ?, movie.edition_label = ?, parent_id = ? WHERE id = ?', [movie_name, alt_title, isEdition, parent_id, movierow.id], function (err,rows) {
+						if (err) throw err;		
+						//updated the title and edition					
+
+					});
+
+				});			
+			}
+			//Update the moviesource info				
+			updateMovieSource(movieResults, movierow.id, movierow.moviesourceid, callback);
+		} else {
+			//if this fails, then 
+			//locate based on title or alt_title and approximate year
+			var query = connection.query('SELECT movie.id, movie.title, movie.year, movie.mpaa, movie.edition_label, movie.parent_id, moviesource.id as moviesourceid FROM movie LEFT JOIN moviesource ON movie.id = moviesource.movie_id AND moviesource.source = \'itunes\' WHERE (movie.title = ? OR movie.alt_title = ?) AND (movie.year >= (?-1) AND movie.year <= (?+1)) ORDER BY movie.release_date ASC;', [movie_name, movie_name, movie_year, movie_year], function (err,rows, fields) {				
+				var parent_id = 0;
+				if (rows.length == 1) {
+					movierow = rows[0];
+				} else if (rows.length > 1) { //see if there is more than one...
+					//see if our movie record is an edition
+					if (isEdition != '') {
+						//it is! then a few possibilities...
+						var matched = false;
+						var potential_parent = new Array();
+						//a) we've already imported this edition, and need to try to match it to the one we've imported before
+						//loop through the movies
+						for (var i = 0;i<rows.length;i++) {
+							//see if isEdition = edition_label
+							if (isEdition == rows[i].edition_label) {									
+								//it is, then movierow = rows[i];
+								movierow = rows[i];
+								matched = true;
+							} else {
+								if (rows[i].parent_id == 0) {
+									potential_parent.push(i);
+								}
+							}			
+						}
+						//end loop
+
+						if (!matched) {
+							//b) we've never imported this edition, but we should go ahead and try to match it up against an original theatrical release
+							//find the movies that parent_id = 0, and preferably mpaa != 0
+							if (potential_parent.length > 0) {
+								parent_id = rows[potential_parent[0]].id; //match it up to the first one by default
+								for (var i = 0;i<potential_parent.length;i++) {
+									if (rows[potential_parent[i]].mpaa == 0) {
+										parent_id = rows[potential_parent[i]].id;
+										break;
+									}
+								}								
+							}								
+						}
+					} else {
+						//its not an edition, and so let's just go with the first one (oldest release date)
+						movierow = rows[0];						
+					}
+				} 
+				if (movierow) {
+					//console.log('Found a movie');
+					//it does, then check to see if there is an iTunes record for it
+					if (movierow.moviesourceid > 0) {
+						//there is, then update it (specifically the moviesource info)
+						//Update the moviesource info				
+						updateMovieSource(movieResults, movierow.id, movierow.moviesourceid, callback);
+					} else {
+						//console.log('there is not, then create the iTunes moviesource record for this movie');
+						updateMovieSource(movieResults, movierow.id, 0, callback);
+					}
+				} else {
+					//console.log('Create the movie');
+					var contentRating = 0;
+					if (movieResults['contentRating'].name == 'G') {
+						contentRating = 1;
+					} else if (movieResults['contentRating'].name == 'PG') {
+						contentRating = 2;
+					} else if (movieResults['contentRating'].name == 'PG-13') {
+						contentRating = 3;
+					} else if (movieResults['contentRating'].name == 'R') {
+						contentRating = 4;
+					} else if (movieResults['contentRating'].name == 'NC-17') {
+						contentRating = 5;
+					}
+
+					var moviedata = {
+						title : movie_name,
+						alt_title : alt_title,
+						year : movie_year,
+						mpaa : contentRating,				//[0=NR,1=G,2=PG,3=PG-13,4=R,5=NC-17]
+						description : '',
+						genre_primary : '',
+						director_id : 0,
+						duration : '',			//	[120]
+						studio : '',
+						budget : '',
+						release_date : movieResults['releaseDate'],
+						box_office : '',
+						edition_label: isEdition,
+						parent_id : parent_id
+					};
+					//console.log(moviedata);										
+					//it doesn't? then create the movie record
+					var query = connection.query('INSERT INTO movie set ? ', moviedata, function(err,result,fields) {				
+						//create the moviesource record as well...				
+						updateMovieSource(movieResults, result.insertId, 0, callback);				
+					});			
+				}
+			
+			});
+
+		}
+	});			
 	//console.log(query.sql);
 }
 
@@ -242,28 +314,23 @@ function updateMovieSource(movieResults, movie_id, moviesource_id, callback) {
 	var rental_price_hd = 0;
 	var purchase_price_hd = 0;
 	var coming_soon = 0;
-	for (var i = 0;i<movieResults['offers'].length;i++) {
-		if (movieResults['offers'][i].requiresHdcp) {
-			//hd
-			if (movieResults['offers'][i]['actionText'].short == 'Buy') {
-				purchase_price_hd = movieResults['offers'][i].price;
-			} else if (movieResults['offers'][i]['actionText'].short == 'Rent') {
-				rental_price_hd = movieResults['offers'][i].price;
-			} else if (movieResults['offers'][i]['actionText'].short == 'Pre-Order') {
-				purchase_price_hd = movieResults['offers'][i].price;
-				coming_soon = 1;
-			}
-		} else {
-			//non-HD
-			if (movieResults['offers'][i]['actionText'].short == 'Buy') {
-				purchase_price = movieResults['offers'][i].price;
-			} else if (movieResults['offers'][i]['actionText'].short == 'Rent') {
-				rental_price = movieResults['offers'][i].price;
-			} else if (movieResults['offers'][i]['actionText'].short == 'Pre-Order') {
-				purchase_price = movieResults['offers'][i].price;
-				coming_soon = 1;
-			}
-		}
+	for (var i = 0;i<movieResults['offers'].length;i++) {	
+		//hd
+		if (movieResults['offers'][i]['actionText'].medium == 'Buy HD') {
+			purchase_price_hd = movieResults['offers'][i].price;
+		} else if (movieResults['offers'][i]['actionText'].medium == 'Rent HD') {
+			rental_price_hd = movieResults['offers'][i].price;
+		} else if (movieResults['offers'][i]['actionText'].medium == 'Pre-Order HD') {
+			purchase_price_hd = movieResults['offers'][i].price;
+			coming_soon = 1;
+		} else if (movieResults['offers'][i]['actionText'].medium == 'Buy') { //non-HD
+			purchase_price = movieResults['offers'][i].price;
+		} else if (movieResults['offers'][i]['actionText'].medium == 'Rent') {
+			rental_price = movieResults['offers'][i].price;
+		} else if (movieResults['offers'][i]['actionText'].medium == 'Pre-Order') {
+			purchase_price = movieResults['offers'][i].price;
+			coming_soon = 1;
+		}		
 	}
 
 	//@@@make sure this movieResults['id'] isn't already in moviesource table...	
@@ -279,17 +346,17 @@ function updateMovieSource(movieResults, movie_id, moviesource_id, callback) {
 					deleting_id = movie_id;
 					movie_id = rows[0].movie_id; //change the reference of this moviesource we have found to be the right movie
 				}
-				var query = connection.query("DELETE FROM movie WHERE id = ? AND active = 0 LIMIT 1;", deleting_id function(err,rows) {
+				var query = connection.query("DELETE FROM movie WHERE id = ? AND active = 0 LIMIT 1;", deleting_id, function(err,rows) {
 					//deleting a duplicate movie 
 
 				});					
 			}
-			if (moviesource_id == 0) {
+			//if (moviesource_id == 0) { //no matter which one we were trying to udpate, we're forcing it to be the first one...
 				moviesource_id = rows[0].id; //we are now updating this moviesource, since there's one with this itunes source id already
-			}
-			if (rows.length > 1) {
+			//}
+			if (rows.length > 1) { //delete the other moviesources
 				for(var i = 1;i<rows.length;i++) {
-					var query = connection.query("DELETE FROM moviesource WHERE id = ?", rows[i], function(err, rows) {
+					var query = connection.query("DELETE FROM moviesource WHERE id = ?", rows[i].id, function(err, rows) {
 						//deleted the duplicate moviesources!
 					});
 				}
@@ -323,24 +390,42 @@ function updateMovieSource(movieResults, movie_id, moviesource_id, callback) {
 	});	
 }
 
+//An edition is a version of an original film (e.g. Director's Cut, etc.).
+//Keywords to match an edition: Director, Unrated, Extended within a () or []
+function detectEdition(movietitle) {
+	movietitle = strip3D(stripYears(movietitle.trim()));
+	var match = movietitle.match(/(\s*(\(|\[).*(director|unrated|extended|edition).*?(\)|\])|\s*(\[)(dubbed|subtitled)(\]))/gi);
+	if (match !== null) {
+		var holdstr = '';
+		for(var j = 0;j<match.length;j++) {
+			holdstr += match[j].trim() + ',';
+		}
+		return holdstr.substr(0, holdstr.length - 1);
+	} else {
+		return '';
+	}
+}
+
 //normalize the movie title (removing additional things like "(2015) or (Watch Now)"")	
 function normalizeMovieTitle(movietitle) {
 	var thereturn = movietitle;
 	thereturn = thereturn.trim();
-			
+	thereturn = strip3D(stripYears(thereturn));
+
+	thereturn = thereturn.replace(/(\s*(\(|\[).*(director|unrated|extended|edition).*?(\)|\])|\s*(\[)(dubbed|subtitled)(\]))/gi, '');
+
 	//if it ends on a right ) and the corresponding left ( doesn't start the string, then cut it from the string
-		if (thereturn[thereturn.length - 1] == ')') {
-			for(var i = (thereturn.length - 1);i>=0;i--) {
-				if (thereturn[i] == '(') {
-					if (i != 0) {
-						thereturn = thereturn.substr(0, i-1);
-						thereturn = thereturn.trim();
-					}
-					break;
+	if ((thereturn[thereturn.length - 1] == ')') || (thereturn[thereturn.length - 1] == ']')) {
+		for(var i = (thereturn.length - 1);i>=0;i--) {
+			if ((thereturn[i] == '(') || (thereturn[i] == '[')) {
+				if (i != 0) {
+					thereturn = thereturn.substr(0, i-1);
+					thereturn = thereturn.trim();
 				}
+				break;
 			}
 		}
-
+	}
 	// if there is a hyphen - directly to the left of the ( then cut that too
 	if (thereturn[thereturn.length - 1] == '-') {
 		thereturn = thereturn.substr(0, thereturn.length - 2);
@@ -353,18 +438,27 @@ function normalizeMovieTitle(movietitle) {
 	if (match !== null) {
 		thereturn = holdsplit[0];
 	}
-
-
+	
 	return thereturn;
+}
+function stripYears(somestr) {	
+	return somestr.replace(/\s*(\(|\[)(\d{4})(\)|\])/, '').trim();
+}
+function strip3D(somestr) {
+	return somestr.replace(/\s*(\(|\[)(\d{1})D(\)|\])/, '').trim();
 }
 
 
-connection.connect();
 
+connection.connect();	
 
 var now = new Date();
 connection.query("SELECT url FROM itunes_urlq WHERE (dateparsed < ? OR dateparsed IS NULL) ORDER by rand() ", now.format("YYYY-MM-DD"), function(err, rows, fields) {
-    if (err) throw err;
+    if (err) {
+    	console.log("Query error: " + err);
+    	//throw err;
+    	return;
+    }    
 
  	var url_queue = new Array(); // 
  	if (rows.length > 0) { 	
---
+++ test.js
@@ -0,0 +1,79 @@
+
+
+function detectEdition(movietitle) {
+	movietitle = strip3D(stripYears(movietitle.trim()));
+	var match = movietitle.match(/(\s*(\(|\[).*(director|unrated|extended).*?(\)|\])|\s*(\[)(dubbed|subtitled)(\]))/gi);
+	if (match !== null) {
+		var holdstr = '';
+		for(var j = 0;j<match.length;j++) {
+			holdstr += match[j].trim() + ',';
+		}
+		return holdstr.substr(0, holdstr.length - 1);
+	} else {
+		return '';
+	}
+}
+
+//normalize the movie title (removing additional things like "(2015) or (Watch Now)"")	
+function normalizeMovieTitle(movietitle) {
+	var thereturn = movietitle;
+	thereturn = thereturn.trim();
+	thereturn = strip3D(stripYears(thereturn));
+
+	thereturn = thereturn.replace(/(\s*(\(|\[).*(director|unrated|extended).*?(\)|\])|\s*(\[)(dubbed|subtitled)(\]))/gi, '');
+
+	//if it ends on a right ) and the corresponding left ( doesn't start the string, then cut it from the string
+		if ((thereturn[thereturn.length - 1] == ')') || (thereturn[thereturn.length - 1] == ']')) {
+			for(var i = (thereturn.length - 1);i>=0;i--) {
+				if ((thereturn[i] == '(') || (thereturn[i] == '[')) {
+					if (i != 0) {
+						thereturn = thereturn.substr(0, i-1);
+						thereturn = thereturn.trim();
+					}
+					break;
+				}
+			}
+		}
+
+	// if there is a hyphen - directly to the left of the ( then cut that too
+	if (thereturn[thereturn.length - 1] == '-') {
+		thereturn = thereturn.substr(0, thereturn.length - 2);
+		thereturn = thereturn.trim();
+	}
+
+	//if there is 'Anniversary' to the right of the - then cut everything to the right of the hyphen -
+	var holdsplit = thereturn.split('-');	
+	var match = holdsplit[0].match(/anniversary/gi);
+	if (match !== null) {
+		thereturn = holdsplit[0];
+	}
+	
+	return thereturn;
+}
+function stripYears(somestr) {	
+	return somestr.replace(/\s*(\(|\[)(\d{4})(\)|\])/, '').trim();
+}
+function strip3D(somestr) {
+	return somestr.replace(/\s*(\(|\[)(\d{1})D(\)|\])/, '').trim();
+}
+
+
+var movie_name = 'The Italian (L\'Italien) [The Italian]';
+//var movie_name = 'Quai des Orfèvres (Jenny Lamour) [Subtitled]';
+//Robin Hood (Unrated Director's Cut) [2010] [Unrated Director's Cut]
+//Robin Hood (Unrated Director's Cut) [2010] [Unrated Director's Cut]
+//The Hunchback of Notre Dame (Silent) [1923]
+
+//see if this is a director's cut, extended edition, or anniversary edition
+var isEdition = detectEdition(movie_name); //blank if not, otherwise the descriptors comma-separated
+
+//normalize the title
+var alt_title = movie_name;
+movie_name = normalizeMovieTitle(movie_name);
+if (alt_title == movie_name) {
+	alt_title = '';
+}
+
+console.log(movie_name);
+console.log(alt_title);
+console.log(isEdition);
